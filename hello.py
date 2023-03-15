dd4d86e920e1f411131a46ff390daaa732652448
Ivan Leskov <ivan.leskov@protonmail.com>
2023-03-15 17:00:43 +0100

    add hello

---
+++ hello.py
@@ -0,0 +1,10 @@
+from cs50 import get_string
+
+answer = get_string("What's you name? ")
+print("hello, " + answer)
+
+answer1 = get_string("What's you name? ")
+print("hello,", answer1)
+
+answer2 = get_string("What's you name? ")
+print(f"hello, {answer2}")
