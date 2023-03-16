16dc9e6cc2d8544ae2b1441409c65a7c27a97631
Ivan Leskov <ivan.leskov@protonmail.com>
2023-03-16 22:56:27 +0100

    add hello

---
+++ sentimental-hello/hello.py
@@ -0,0 +1,8 @@
+# Prompt the user for their name and save the input to a variable called "name"
+name = input("What is your name?\n")
+
+# Build a personalized greeting by concatenating the string "hello, " with the user's name
+greeting = "hello, " + name
+
+# Print the personalized greeting to the console
+print(greeting)
